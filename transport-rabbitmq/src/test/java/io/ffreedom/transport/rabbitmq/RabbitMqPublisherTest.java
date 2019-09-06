ce2e4895477464bf620a64d0a913b0b8a56dd619
yellow013 <wk9988@gmail.com>
2019-09-06 21:40:09 +0800

    update rabbit test

--- transport-rabbitmq/src/test/java/io/ffreedom/transport/rabbitmq/RabbitMqPublisherTest.java
+++ transport-rabbitmq/src/test/java/io/ffreedom/transport/rabbitmq/RabbitMqPublisherTest.java
@@ -8,18 +8,7 @@ public class RabbitMqPublisherTest {
 
 	public static void main(String[] args) {
 
-		ConnectionConfigurator connectionConfigurator = ConnectionConfigurator.configuration("10.0.64.201", 5672,
-				"global", "global2018", "report");
-
-		RabbitMqPublisher publisher = new RabbitMqPublisher("test-publisher",
-				PublisherConfigurator.configuration(connectionConfigurator)
-				
-				
-				);
-
-		new RabbitMqReceiver("test-receiver",
-				ReceiverConfigurator.configuration(connectionConfigurator).setReceiveQueue(""),
-				byteMsg -> System.out.println(new String(byteMsg))).receive();
+	
 
 	}
 
