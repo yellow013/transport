32c4c11038f6fcf10e739982a54d892fef142864
yellow013 <wk9988@gmail.com>
2019-09-09 23:00:47 +0800

    update consumer

--- transport-rabbitmq/src/main/java/io/ffreedom/transport/rabbitmq/consumer/QueueMessageSerializable.java
+++ transport-rabbitmq/src/main/java/io/ffreedom/transport/rabbitmq/consumer/QueueMessageSerializable.java
@@ -6,15 +6,17 @@ import java.util.function.Function;
  * @author xuejian.sun
  * @date 2018/11/20 14:44
  */
-public interface QueueMessageSerializable<R> extends Function<byte[],R> {
+@FunctionalInterface
+public interface QueueMessageSerializable<R> extends Function<byte[], R> {
 
-    /**
-     * convert byte[] to R
-     *
-     * @param bytes bytes
-     * @return R
-     */
-    default R serialize(byte[] bytes){
-        return apply(bytes);
-    }
+	/**
+	 * convert byte[] to R
+	 *
+	 * @param bytes bytes
+	 * @return R
+	 */
+	default R serialize(byte[] bytes) {
+		return apply(bytes);
+	}
+	
 }
--- transport-rabbitmq/src/main/java/io/ffreedom/transport/rabbitmq/consumer/RefreshNowEvent.java
+++ transport-rabbitmq/src/main/java/io/ffreedom/transport/rabbitmq/consumer/RefreshNowEvent.java
@@ -6,8 +6,11 @@ import java.util.function.Predicate;
  * @author xuejian.sun
  * @date 2019-01-17 15:46
  */
+@FunctionalInterface
 public interface RefreshNowEvent<T> extends Predicate<T> {
+
 	default boolean flushNow(T obj) {
 		return test(obj);
 	}
+
 }
