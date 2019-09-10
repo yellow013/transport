fc0d5837eb892decb25194603b8211164338c092
yellow013 <wk9988@gmail.com>
2019-09-10 10:44:24 +0800

    update test

--- transport-rabbitmq/src/test/java/io/ffreedom/transport/rabbitmq/RabbitMqDeclareDSLTest.java
+++ transport-rabbitmq/src/test/java/io/ffreedom/transport/rabbitmq/RabbitMqDeclareDSLTest.java
@@ -2,11 +2,15 @@ package io.ffreedom.transport.rabbitmq;
 
 import org.junit.Test;
 
+import io.ffreedom.transport.rabbitmq.declare.ExchangeDeclare;
+
 public class RabbitMqDeclareDSLTest {
 
 	@Test
 	public void test() {
-	
+		
+		ExchangeDeclare.fanoutExchange("FAN_T1");
+		
 	
 	}
 
--- transport-rabbitmq/src/test/java/io/ffreedom/transport/rabbitmq/RabbitMqPublisherTest.java
+++ transport-rabbitmq/src/test/java/io/ffreedom/transport/rabbitmq/RabbitMqPublisherTest.java
@@ -1,14 +1,32 @@
 package io.ffreedom.transport.rabbitmq;
 
+import java.util.Arrays;
+
 import io.ffreedom.transport.rabbitmq.config.ConnectionConfigurator;
 import io.ffreedom.transport.rabbitmq.config.PublisherConfigurator;
-import io.ffreedom.transport.rabbitmq.config.ReceiverConfigurator;
+import io.ffreedom.transport.rabbitmq.declare.BaseEntity.Queue;
+import io.ffreedom.transport.rabbitmq.declare.ExchangeDeclare;
 
 public class RabbitMqPublisherTest {
 
 	public static void main(String[] args) {
 
-	
+		ConnectionConfigurator connectionConfigurator = ConnectionConfigurator
+				.configuration("10.0.64.201", 5672, "global", "global2018").build();
+
+		PublisherConfigurator publisherConfigurator = PublisherConfigurator
+				.configuration(connectionConfigurator,
+						ExchangeDeclare.fanoutExchange("TEST_DIR").declareBindingQueue(
+								Arrays.asList(Queue.declare("TEST_D1")), Arrays.asList("K1", "K2")))
+				.setDefaultRoutingKey("K1").build();
+
+		RabbitMqPublisher publisher = new RabbitMqPublisher("TEST_PUB", publisherConfigurator);
+
+		publisher.publish(new String("To_K1").getBytes());
+		publisher.publish("K1", new String("To_K1_0").getBytes());
+		publisher.publish("K2", new String("To_K2").getBytes());
+
+		publisher.destroy();
 
 	}
 
