574213f6d7f86c343286fab755954e9304466c82
yellow013 <wk9988@gmail.com>
2020-02-06 12:24:33 +0800

    update test

--- transport-rabbitmq/src/test/java/io/mercury/transport/rabbitmq/RabbitMqPublisherTest.java
+++ transport-rabbitmq/src/test/java/io/mercury/transport/rabbitmq/RabbitMqPublisherTest.java
@@ -1,5 +1,6 @@
 package io.mercury.transport.rabbitmq;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import io.mercury.transport.rabbitmq.configurator.RmqConnection;
@@ -11,21 +12,21 @@ public class RabbitMqPublisherTest {
 
 	public static void main(String[] args) {
 
-		RmqConnection connectionConfigurator = RmqConnection
-				.configuration("10.0.64.201", 5672, "global", "global2018").build();
+		RmqConnection connectionConfigurator = RmqConnection.configuration("10.0.64.201", 5672, "global", "global2018")
+				.build();
 
 		RmqPublisherConfigurator publisherConfigurator = RmqPublisherConfigurator
 				.configuration(connectionConfigurator, ExchangeDeclare.fanout("TEST_DIR")
 						.bindingQueue(Arrays.asList(Queue.named("TEST_D1")), Arrays.asList("K1", "K2")))
 				.defaultRoutingKey("K1").build();
 
-		RabbitMqPublisher publisher = new RabbitMqPublisher("TEST_PUB", publisherConfigurator);
-
-		publisher.publish(new String("To_K1").getBytes());
-		publisher.publish("K1", new String("To_K1_0").getBytes());
-		publisher.publish("K2", new String("To_K2").getBytes());
-
-		publisher.destroy();
+		try (RabbitMqPublisher publisher = new RabbitMqPublisher("TEST_PUB", publisherConfigurator)) {
+			publisher.publish(new String("To_K1").getBytes());
+			publisher.publish("K1", new String("To_K1_0").getBytes());
+			publisher.publish("K2", new String("To_K2").getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
