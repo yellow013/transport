e6cd7fb3285edb63a7e44e33fb1a205d2ddeba76
yellow013 <wk9988@gmail.com>
2020-04-17 23:35:48 +0800

    update api publisher

--- transport-core/src/main/java/io/mercury/transport/core/api/Publisher.java
+++ transport-core/src/main/java/io/mercury/transport/core/api/Publisher.java
@@ -1,14 +1,14 @@
 package io.mercury.transport.core.api;
 
 import io.mercury.transport.core.TransportModule;
-import io.mercury.transport.core.exception.FailPublishException;
+import io.mercury.transport.core.exception.PublishFailedException;
 
 public interface Publisher<T> extends TransportModule {
 
 	// Publish to default location
-	void publish(T msg) throws FailPublishException;
+	void publish(T msg) throws PublishFailedException;
 
 	// Publish to target location
-	void publish(String target, T msg) throws FailPublishException;
+	void publish(String target, T msg) throws PublishFailedException;
 
 }
