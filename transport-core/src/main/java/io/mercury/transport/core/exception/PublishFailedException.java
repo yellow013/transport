b207df09cc2f6b0c660a9181c1a2a283e4423f18
yellow013 <wk9988@gmail.com>
2020-04-17 23:35:22 +0800

    update exception

--- transport-core/src/main/java/io/mercury/transport/core/exception/PublishFailedException.java
+++ transport-core/src/main/java/io/mercury/transport/core/exception/PublishFailedException.java
@@ -1,21 +1,21 @@
 package io.mercury.transport.core.exception;
 
-public class FailPublishException extends RuntimeException {
+public class PublishFailedException extends RuntimeException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4306166538549739230L;
 
-	public FailPublishException(String message) {
+	public PublishFailedException(String message) {
 		super(message);
 	}
 
-	public FailPublishException(Throwable throwable) {
+	public PublishFailedException(Throwable throwable) {
 		super(throwable);
 	}
 
-	public FailPublishException(String message, Throwable throwable) {
+	public PublishFailedException(String message, Throwable throwable) {
 		super(message, throwable);
 	}
 
